fe87c3b41e2b4106a934a2a8482a202aa2e36422
Tgarg18 <tushargarg70091@gmail.com>
2024-07-05 22:05:00 +0530

    minor-change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
     return (
         <>
             <Navbar1 />
-            <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../src/assets/background.jpg')`,backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover'}}>
+            <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../../public/background.jpg')`,backgroundPosition: 'center',backgroundRepeat: 'no-repeat',backgroundSize: 'cover'}}>
                 <form onSubmit={handleSubmit(onSubmit)} className='w-1/4 border-2 border-black rounded-xl py-3 px-10 shadow-2xl shadow-black'>
                     <h2 className='text-2xl text-center font-bold'>Login to ManageMate</h2>
                     <NavLink to={'/signup'} className={`w-full hover:underline`} draggable={false}>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -95,7 +95,7 @@ const Signup = () => {
     <>
       <Navbar1 />
       {stage === 1 ? (
-        <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../src/assets/background.jpg')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../../public/background.jpg')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
           <form onSubmit={handleSubmit(onSubmit)} className='w-1/4 border-2 border-black rounded-xl py-3 px-10 shadow-2xl  shadow-black'>
             <h2 className='text-2xl text-center font-semibold'>Signup to ManageMate</h2>
             <NavLink to={'/login'} className={`w-full hover:underline`} draggable={false}>
@@ -130,7 +130,7 @@ const Signup = () => {
           </form>
         </div>
       ) : (
-        <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../src/assets/background.jpg')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className='w-full pt-10 h-[calc(92vh)] flex justify-center items-center' style={{ backgroundImage: `url('../../public/background.jpg')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
           <form className='w-3/4 border-2 border-black rounded-xl py-3 px-10 shadow-2xl shadow-black'>
             <div className='flex items-center'>
               <label htmlFor="bio" className='font-semibold'>Write a short bio</label>
